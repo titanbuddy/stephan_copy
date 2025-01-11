530b2f8df68ae25487268e324499137db0936768
titanbuddy <buddyrocstar123@gmail.com>
2025-01-11 13:49:09 +0500

    lenis scrolling!

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+document.querySelectorAll('a[href^="#"]').forEach((el) => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault()
+      const id = el.getAttribute('href')?.slice(1)
+      if (!id) return
+      const target = document.getElementById(id)
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' })
+      }
+    })
+  })
+requestAnimationFrame(raf)
 const burger = document.querySelector('.burger');
 const closeNav = document.querySelector('.close');
 const mobileNav = document.querySelector('.mobile-nav');
